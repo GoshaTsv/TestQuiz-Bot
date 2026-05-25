3d09ae19c010edb63f97fc6c26187ee537937b6f
Cupmaster <gosha.tswetkov@gmail.com>
2026-05-25 19:14:37 +0300

    dashjdasjhkads

--- src/main/java/org/example/bot/TelegramBot.java
+++ src/main/java/org/example/bot/TelegramBot.java
@@ -690,9 +690,8 @@ public class TelegramBot extends TelegramLongPollingBot {
                 for (Boolean b: user.getUserAnswers().values()){
                     String userAnswer = user.getUserAnswers().keySet().toArray()[qIndex].toString();
                     String correntAnswer = User.getCorrectAnswerForQuestion(user.getCurrentQuiz().getTest().questions.get(qIndex));
-                    int question = user.getUserAnswers().keySet().stream().toList().indexOf(String.valueOf(user.getUserAnswers().keySet().stream().filter(x -> x.equalsIgnoreCase(correntAnswer)).findFirst()));
-                    sendMessage("Вопрос #" + question + ". \nОтвет вашего ученика: " + userAnswer + "\nПравильный ответ: " + correntAnswer, quiz.getTeacherId());
-
+                    sendMessage("Вопрос #" + qIndex+1 + ". \nОтвет вашего ученика: " + userAnswer + "\nПравильный ответ: " + correntAnswer, quiz.getTeacherId());
+                    qIndex++;
                 }
                 userCurrent.setQuizState(-1);
                 userCurrent.setCurrentQuiz(null);
