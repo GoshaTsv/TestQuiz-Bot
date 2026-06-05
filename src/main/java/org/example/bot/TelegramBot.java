6b4680755544019fabc168063349d7f57b90c709
SuperWarden <plonisan6@gmail.com>
2026-06-05 15:24:16 +0300

    Removed user == null check in sendMessage

--- src/main/java/org/example/bot/TelegramBot.java
+++ src/main/java/org/example/bot/TelegramBot.java
@@ -438,12 +438,6 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     public Integer sendMessage(String msg, long chatId) {
         AtomicReference<Integer> messageId = new AtomicReference<>();
-        User user = users.stream().filter(x -> x.getChatId() == chatId).findFirst().orElse(null);
-        if (user == null){
-            alertMessage("Не получилось найти пользователя...", chatId, 10000, user);
-            return -1;
-        }
-
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
         sendMessage.setText(msg);
