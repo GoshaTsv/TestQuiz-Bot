85acf21497e8f713670ed331a5478dc9baacef54
GoshaTsv <gosha.tswetkov@gmail.com>
2026-06-08 13:07:17 +0300

    removed deleting fucking up the entire quiz

--- src/main/java/org/example/bot/TelegramBot.java
+++ src/main/java/org/example/bot/TelegramBot.java
@@ -314,13 +314,8 @@ public class TelegramBot extends TelegramLongPollingBot {
 
                     for (int i = 0; i < variants.size(); i++)
                         callbacks.add("ans_" + i);
-                    if (user.getCurrentQuizPhotoId() != -1 || user.getCurrentQuizPhotoId() != null){
-                        deleteMessage(user.getCurrentQuizPhotoId(), chatId);
-                        if (!saveUser(user))
-                            sendMessage("Не удалось обновить состояние пользователя.", chatId);
-                    }
                     Integer photoId = sendPhoto(question, user.getChatId(), user.getCurrentQuizPhotoId());
-                    Integer quizMessageId = sendMessage("QВопрос #" + user.getQuizState() + ": " + question.getQuestion(), chatId, variants, callbacks, user.getCurrentQuizMessageId());
+                    Integer quizMessageId = sendMessage("Вопрос #" + user.getQuizState() + ": " + question.getQuestion(), chatId, variants, callbacks, user.getCurrentQuizMessageId());
                     user.setPrevType("var");
 
 
@@ -350,11 +345,6 @@ public class TelegramBot extends TelegramLongPollingBot {
                         Thread.sleep(250);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
-                    }
-                    if (user.getCurrentQuizPhotoId() != -1 || user.getCurrentQuizPhotoId() != null){
-                        deleteMessage(user.getCurrentQuizPhotoId(), chatId);
-                        if (!saveUser(user))
-                            sendMessage("Не удалось обновить состояние пользователя.", chatId);
                     }
                     Integer photoId = sendPhoto(question, user.getChatId(), user.getCurrentQuizPhotoId());
                     Integer quizMessageId = sendMessage("Вопрос #" + user.getQuizState() + ": " + question.getQuestion(), chatId, user.getCurrentQuizMessageId());
