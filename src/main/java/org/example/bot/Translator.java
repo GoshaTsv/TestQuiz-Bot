da9f8498dd154ea1ddb2617fe856ad9dfa73651d
SuperWarden <plonisan6@gmail.com>
2026-06-10 21:03:36 +0300

    Fixed translate?

--- src/main/java/org/example/bot/Translator.java
+++ src/main/java/org/example/bot/Translator.java
@@ -1,11 +1,10 @@
 package org.example.bot;
 
 import org.springframework.context.MessageSource;
-import org.springframework.context.NoSuchMessageException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import java.util.Locale;
 
-@Service
+@Component
 public class Translator {
     private final MessageSource messageSource;
 
@@ -14,23 +13,10 @@ public class Translator {
     }
 
     public String getTranslatedText(String rawString, String lang, Object... args) {
-        if (rawString == null || rawString.isEmpty())
+        if (rawString == null || rawString.isBlank()) {
             return "";
-
-        if (lang == null || "ru".equalsIgnoreCase(lang))
-            return args.length > 0 ? String.format(rawString, args) : rawString;
-
-        if ("by".equalsIgnoreCase(lang))
-            lang = "be";
-
-        System.out.println("Lang:" + lang);
-
-        Locale locale = Locale.forLanguageTag(lang);
-
-        try {
-            return messageSource.getMessage(rawString, args, locale);
-        } catch (NoSuchMessageException e) {
-            return args.length > 0 ? String.format(rawString, args) : rawString;
         }
+        Locale locale = Locale.forLanguageTag(lang);
+        return messageSource.getMessage(rawString, args, locale);
     }
 }
