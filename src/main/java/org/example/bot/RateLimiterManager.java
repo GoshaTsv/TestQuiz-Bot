cdc5c004c9907922d5d1ed84d25c6d65562a0774
SuperWarden <plonisan6@gmail.com>
2026-06-11 20:57:07 +0300

    Added more debug, fixed some bugs

--- src/main/java/org/example/bot/RateLimiterManager.java
+++ src/main/java/org/example/bot/RateLimiterManager.java
@@ -11,8 +11,8 @@ public class RateLimiterManager {
     private final Map<Long, Bucket> cache = new ConcurrentHashMap<>();
 
     private Bucket createNewBucket() {
-        Bandwidth shortTermLimit = Bandwidth.classic(3, Refill.intervally(3, Duration.ofSeconds(1)));
-        Bandwidth longTermLimit = Bandwidth.classic(20, Refill.intervally(1, Duration.ofSeconds(3)));
+        Bandwidth shortTermLimit = Bandwidth.classic(3, Refill.intervally(3, Duration.ofSeconds(2)));
+        Bandwidth longTermLimit = Bandwidth.classic(5, Refill.intervally(1, Duration.ofSeconds(3)));
 
         return Bucket.builder()
                 .addLimit(shortTermLimit)
